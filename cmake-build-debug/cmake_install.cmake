7783bf24969598525e1d2e7f9e1495058b12fc43
Sage Girl <sagegirl@ge-k6.kzn.21-school.ru>
2023-02-05 13:34:46 +0300

    all_files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/sagegirl/myProjects/SmartCalc
+# Install script for directory: /Users/sagegirl/myProjects/s21_calc_new
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/sagegirl/myProjects/SmartCalc/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/sagegirl/myProjects/s21_calc_new/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
