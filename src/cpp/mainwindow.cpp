9449077d3b451a58ea7fbaa095b8ec160df7a80e
Sage Girl <sagegirl@pr-h4.kzn.21-school.ru>
2023-02-20 20:09:45 +0300

    ui

--- src/cpp/mainwindow.cpp
+++ src/cpp/mainwindow.cpp
@@ -4,18 +4,9 @@
 
 #include "./ui_mainwindow.h"
 
-enum previous_symbol { num, op, dot, open_brace, close_brace, x_num };
-
-int prev_sym = -1;
-int dot_in_num = 0;
-int count_close_braces = 0;
-int count_open_braces = 0;
-double x_value = 0.0;
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
-
   connect(ui->pushButton_0, SIGNAL(clicked()), this, SLOT(num_button()));
   connect(ui->pushButton_1, SIGNAL(clicked()), this, SLOT(num_button()));
   connect(ui->pushButton_2, SIGNAL(clicked()), this, SLOT(num_button()));
@@ -51,6 +42,88 @@ MainWindow::MainWindow(QWidget *parent)
   connect(ui->pushButton_clear, SIGNAL(clicked()), this, SLOT(clear()));
   connect(ui->pushButton_RES, SIGNAL(clicked()), this, SLOT(equal_button()));
   connect(ui->pushButton_graph, SIGNAL(clicked()), this, SLOT(graph_button()));
+  connect(ui->pushButton_C, SIGNAL(clicked()), this, SLOT(backspace_button()));
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *e)
+{
+    if (e->key() == Qt::Key_0) ui->pushButton_0->click();
+    else if (e->key() == Qt::Key_1) ui->pushButton_1->click();
+    else if (e->key() == Qt::Key_2) ui->pushButton_2->click();
+    else if (e->key() == Qt::Key_3) ui->pushButton_3->click();
+    else if (e->key() == Qt::Key_4) ui->pushButton_4->click();
+    else if (e->key() == Qt::Key_5) ui->pushButton_5->click();
+    else if (e->key() == Qt::Key_6) ui->pushButton_6->click();
+    else if (e->key() == Qt::Key_7) ui->pushButton_7->click();
+    else if (e->key() == Qt::Key_8) ui->pushButton_8->click();
+    else if (e->key() == Qt::Key_9) ui->pushButton_9->click();
+    else if (e->key() == Qt::Key_Minus) ui->pushButton_min->click();
+    else if (e->key() == Qt::Key_Plus) ui->pushButton_plus->click();
+    else if (e->key() == Qt::Key_Slash) ui->pushButton_div->click();
+    else if (e->key() == Qt::Key_Asterisk) ui->pushButton_mul->click();
+    else if (e->key() == Qt::Key_Equal) ui->pushButton_RES->click();
+    else if (e->key() == Qt::Key_ParenLeft) ui->pushButton_OB->click();
+    else if (e->key() == Qt::Key_ParenRight) ui->pushButton_CB->click();
+    else if (e->key() == Qt::Key_AsciiCircum) ui->pushButton_pow->click();
+    else if (e->key() == Qt::Key_Backspace) ui->pushButton_C->click();
+    else if (e->key() == Qt::Key_Period) ui->pushButton_dot->click();
+    else if (e->key() == Qt::Key_Return) ui->pushButton_RES->click();
+}
+
+void MainWindow::backspace_button() {
+    QString text = ui->label->text();
+    text.chop(1);
+    ui->label->setText(text);
+    change_prev_sym();
+}
+
+void MainWindow::change_prev_sym() {
+    if (ui->label->text().size() != 0) {
+        prev_sym_int = ui->label->text()[ui->label->text().size() - 1].unicode();
+        switch (prev_sym_int) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                prev_sym = num;
+                break;
+            case 'n':
+            case 't':
+            case 's':
+            case 'g':
+            case '(':
+                prev_sym = open_brace;
+                break;
+            case 'x':
+                prev_sym = x_num;
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                prev_sym = op;
+                break;
+            case 'd':
+                if (ui->label->text().endsWith("mod"))
+                    prev_sym = op;
+                else
+                    prev_sym = open_brace;
+                break;
+            case '.':
+                prev_sym = dot;
+                break;
+            case ')':
+                prev_sym = close_brace;
+            default:
+                break;
+        }
+    }
 }
 
 MainWindow::~MainWindow() { delete ui; }
@@ -139,32 +212,32 @@ void MainWindow::close_brace_button() {
 }
 
 void MainWindow::equal_button() {
-  if (ui->label->text().size() != 0) {
+  if (ui->label->text().size() != 0 && ui->label->text().size() <= 255) {
     if (prev_sym == num || prev_sym == close_brace || prev_sym == x_num) {
       double res = 0.0;
       x_value = 0.0;
-//      if (ui->Xline->text().size() != 0) x_value = ui->Xline->text().toDouble();
-//      QString qstring = ui->label->text();
-//      char *str = new char(qstring.length());
-//      QByteArray barr = qstring.toLatin1();
-//      strlcpy(str, barr, qstring.length() + 1);
+      if (ui->Xline->text().size() != 0) x_value = ui->Xline->text().toDouble();
       QString needle = ui->label->text();
       QByteArray ba_x = needle.toLocal8Bit();
       char *str = ba_x.data();
 
       int error = MainCalc(str, &res, x_value);
-      delete (str);
       if (error) {
         QMessageBox::about(this, "Invalid expression", "Invalid input");
+        ui->label->setText("0");
       } else {
         QString valueAsString = QString::number(res, 'g', 15);
         ui->label->setText(valueAsString);
       }
-//      if (res == 0) prev_sym = -1;
-//      if (res != (int)res) dot_in_num = 1;
+      if (res == 0) prev_sym = -1;
+      if (res != (int)res) dot_in_num = 1;
     } else {
       QMessageBox::about(this, "Invalid expression", "Invalid input");
+      ui->label->setText("0");
     }
+  } else {
+      QMessageBox::about(this, "Invalid expression", "Invalid input");
+      ui->label->setText("0");
   }
 }
 
@@ -180,16 +253,20 @@ void MainWindow::graph_button() {
     ui->widget->yAxis->setRange(y_begin, y_end);
     h = 0.1;
     double Y = 0.0;
-    char *str = new char(ui->label->text().length());
-    QByteArray byte_arr = ui->label->text().toLatin1();
-    strlcpy(str, byte_arr, ui->label->text().length() + 1);
+    QString needle = ui->label->text();
+    QByteArray ba_x = needle.toLocal8Bit();
+    char *str = ba_x.data();
+
     int error = OK;
     for (double X_ = x_begin; X_ < x_end && !error; X_ += h) {
       error = MainCalc(str, &Y, X_);
       x.push_back(X_);
       y.push_back(Y);
     }
-    if (error) QMessageBox::about(this, "Invalid expression", "Invalid input");
+    if (error) {
+        QMessageBox::about(this, "Invalid expression", "Invalid input");
+        ui->label->setText("0");
+    }
     ui->widget->addGraph();
     ui->widget->graph(0)->addData(x, y);
     ui->widget->replot();
--- src/cpp/mainwindow.h
+++ src/cpp/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QVector>
+#include <QRegularExpressionValidator>
+#include <QValidator>
 
 extern "C" {
 #include "../smartcalc.h"
@@ -19,6 +21,14 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    int prev_sym = -1;
+    int prev_sym_int = 0;
+    QChar prev_sym_QChar;
+    int dot_in_num = 0;
+    int count_close_braces = 0;
+    int count_open_braces = 0;
+    double x_value = 0.0;
+    enum previous_symbol { num, op, dot, open_brace, close_brace, x_num };
 
 
 private slots:
@@ -33,6 +43,9 @@ private slots:
     void x_button();
     void equal_button();
     void graph_button();
+    void backspace_button();
+    void change_prev_sym();
+    void keyPressEvent(QKeyEvent *);
 
 private:
     Ui::MainWindow *ui;
